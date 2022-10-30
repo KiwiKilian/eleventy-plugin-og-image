14f67856d2b27934627fa8ac2513ed65d05ac985
Kilian Finger <hey@kilianfinger.com>
2022-10-30 08:23:35 +0100

    fix: linting errors

--- postinstall.js
+++ postinstall.js
@@ -2,9 +2,12 @@ const esbuild = require('esbuild');
 
 esbuild
   .build({
+    // eslint-disable-next-line node/no-missing-require
     entryPoints: [require.resolve('satori-html')],
     outfile: 'build/satori-html.js',
     bundle: true,
     platform: 'node',
   })
-  .catch(() => process.exit(1));
+  .catch(() => {
+    throw new Error("eleventy-plugin-og-image couldn't compile satori-html");
+  });
