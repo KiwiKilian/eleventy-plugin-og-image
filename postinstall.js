86422c49b8dad55830b1f0c991c118a473821a10
Kilian Finger <hey@kilianfinger.com>
2023-02-20 21:19:13 +0100

    feat: use yoga-wasm-web

--- postinstall.js
+++ postinstall.js
@@ -3,6 +3,18 @@ const esbuild = require('esbuild');
 esbuild
   .build({
     // eslint-disable-next-line node/no-missing-require
+    entryPoints: [require.resolve('yoga-wasm-web')],
+    outfile: 'build/yoga-wasm-web.js',
+    bundle: true,
+    platform: 'node',
+  })
+  .catch(() => {
+    throw new Error("eleventy-plugin-og-image couldn't compile yoga-wasm-web");
+  });
+
+esbuild
+  .build({
+    // eslint-disable-next-line node/no-missing-require
     entryPoints: [require.resolve('satori-html')],
     outfile: 'build/satori-html.js',
     bundle: true,
--- src/renderOgImage.js
+++ src/renderOgImage.js
@@ -1,11 +1,11 @@
+const fs = require('fs');
 const { File } = require('@11ty/eleventy/src/Plugins/RenderPlugin');
 const { default: satori, init } = require('satori/wasm');
-const yoga = require('yoga-layout-prebuilt');
 const { Resvg } = require('@resvg/resvg-js');
 // eslint-disable-next-line node/no-unpublished-require
+const { default: initYoga } = require('../build/yoga-wasm-web');
+// eslint-disable-next-line node/no-unpublished-require
 const { html: htmlToSatori } = require('../build/satori-html');
-
-init(yoga);
 
 module.exports = {
   /**
@@ -15,6 +15,9 @@ module.exports = {
    * @param { import('@11ty/eleventy/src/TemplateConfig') } [templateConfig]
    * */
   async renderOgImage(inputPath, data, satoriOptions, templateConfig) {
+    const yoga = await initYoga(fs.readFileSync('../node_modules/yoga-wasm-web/dist/yoga.wasm'));
+    init(yoga);
+
     const html = await (await File(inputPath, { templateConfig }))(data);
     const svg = await satori(htmlToSatori(html), satoriOptions);
     const resvg = new Resvg(svg, { font: { loadSystemFonts: false } });
