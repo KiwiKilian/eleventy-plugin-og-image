cefecb3fc2b94a00608b73d58df000f309a94815
Kilian Finger <hey@kilianfinger.com>
2023-06-01 21:36:07 +0200

    feat: add development mode (#123)
    BREAKING CHANGE: switch `renderOgImage` parameters to object style

--- src/renderOgImage.js
+++ src/renderOgImage.js
@@ -2,10 +2,14 @@ const fs = require('fs');
 const { File } = require('@11ty/eleventy/src/Plugins/RenderPlugin');
 const { default: satori, init } = require('satori/wasm');
 const { Resvg } = require('@resvg/resvg-js');
-// eslint-disable-next-line node/no-unpublished-require
+
+/* eslint-disable node/no-unpublished-require */
 const { default: initYoga } = require('../build/yoga-wasm-web');
-// eslint-disable-next-line node/no-unpublished-require
 const { html: htmlToSatori } = require('../build/satori-html');
+/* eslint-enable node/no-unpublished-require */
+
+const Yoga = initYoga(fs.readFileSync(require.resolve('yoga-wasm-web/dist/yoga.wasm')));
+init(Yoga);
 
 module.exports = {
   /**
@@ -13,15 +17,13 @@ module.exports = {
    * @param { Record<string, any> } [data]
    * @param { import('satori').SatoriOptions } satoriOptions
    * @param { import('@11ty/eleventy/src/TemplateConfig') } [templateConfig]
+   *
+   * @returns { Promise<{ html: string, svg: string, pngBuffer: Buffer }> }
    * */
-  async renderOgImage(inputPath, data, satoriOptions, templateConfig) {
-    const yoga = await initYoga(fs.readFileSync(require.resolve('yoga-wasm-web/dist/yoga.wasm')));
-    init(yoga);
-
+  async renderOgImage({ inputPath, data, satoriOptions, templateConfig }) {
     const html = await (await File(inputPath, { templateConfig }))(data);
     const svg = await satori(htmlToSatori(html), satoriOptions);
-    const resvg = new Resvg(svg, { font: { loadSystemFonts: false } });
-    const pngBuffer = resvg.render().asPng();
+    const pngBuffer = new Resvg(svg, { font: { loadSystemFonts: false } }).render().asPng();
 
     return { html, svg, pngBuffer };
   },
--- test/renderOgImage.test.js
+++ test/renderOgImage.test.js
@@ -2,10 +2,14 @@ const test = require('ava');
 const { renderOgImage } = require('../src/renderOgImage');
 
 test('works with example', async (t) => {
-  const { html, svg, pngBuffer } = await renderOgImage('./test/og-test.og.njk', undefined, {
-    width: 1200,
-    height: 630,
-    fonts: [],
+  const { html, svg, pngBuffer } = await renderOgImage({
+    inputPath: './test/og-test.og.njk',
+    data: undefined,
+    satoriOptions: {
+      width: 1200,
+      height: 630,
+      fonts: [],
+    },
   });
 
   t.snapshot(html);
