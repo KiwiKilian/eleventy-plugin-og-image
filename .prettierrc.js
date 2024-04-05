ec72c000cc756389b135c4c20ba4cd491ae6df83
Kilian Finger <hey@kilianfinger.com>
2024-04-05 22:39:45 +0200

    chore: add prettier-plugin-jinja-template

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,15 @@
 import prettierConfig from '@kiwikilian/prettier-config' with { type: 'json' };
 
+/** @type {import('prettier').Config} */
 export default {
   ...prettierConfig,
-
-  plugins: ['prettier-plugin-jsdoc'],
+  plugins: ['prettier-plugin-jsdoc', 'prettier-plugin-jinja-template'],
+  overrides: [
+    {
+      files: ['**/*.njk'],
+      options: {
+        parser: 'jinja-template',
+      },
+    },
+  ],
 };
